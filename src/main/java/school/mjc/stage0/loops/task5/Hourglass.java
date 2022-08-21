c9190e91ac01cb18acb749f8fc3fb8e357a6035c
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-21 12:10:43 +0500

    Update Hourglass.java

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,32 +2,28 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-	    
-        for ( int i = 0; i < height - 1; i++ )
-        {
-            for ( int j = 0; j < i; j++ )
-            {
-                System.out.print( " " );
-            }
-            for ( int k = height - i; k > 0; k-- )
-            {
-                System.out.print( "8 " );
-            }
-            System.out.println();
-        }
-        for ( int i = 0; i < height; i++ )
-        {
-            for ( int j = height - i; j > 1; j-- )
-            {
-                System.out.print( "" );
-            }
-            for ( int k = 0; k < i + 1; k++ )
-            {
-                System.out.print( "8 " );
-            }
-            System.out.println();
-        }
+	
+
+for(int i=0; i < height/2; i++ ) {
+  for(int j = height - i; j < height; j++) {
+     System.out.print(" ");
+  }
+  for(int j = 0; j < height - 2*i; j++) {
+     System.out.print("8");
+  }
+  System.out.println();
+}
 
 
+for(int i=nRows/2 -1; i >=0; i-- )  // based on first half, in reverse order
+{
+  for(int j = height - i; j < height; j++) {
+     System.out.print(" ");
+  }
+  for(int j = 0; j < height - 2*i; j++) {
+     System.out.print("8");
+  }
+  System.out.println();
+}
     }
 }
