5122817ce1f13379b60809161b6eec0041bbc9a7
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-21 11:57:03 +0500

    Update Hourglass.java

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,38 +2,32 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-  
-    int dimension = (height * 2) - 1, space = 0, stars = height - 1, printed = 0;
+	    
+        for ( int i = 0; i < height - 1; i++ )
+        {
+            for ( int j = 0; j < i; j++ )
+            {
+                System.out.print( " " );
+            }
+            for ( int k = height - i; k > 0; k-- )
+            {
+                System.out.print( "8 " );
+            }
+            System.out.println();
+        }
+        for ( int i = 0; i < height; i++ )
+        {
+            for ( int j = height - i; j > 1; j-- )
+            {
+                System.out.print( "" );
+            }
+            for ( int k = 0; k < i + 1; k++ )
+            {
+                System.out.print( "8 " );
+            }
+            System.out.println();
+        }
 
-    for(int i=0; i<dimension; i++) {
-        int actual = space;
-        for (int j=dimension; j > 0; j--) {
-            if(actual > 0) {
-                System.out.print(" ");
-                actual--;
-            }
-            else {
-                System.out.print("8");
-                if(stars==printed) {
-                    actual = space;
-                    printed = 0;
-                } else {
-                    actual = 1;
-                    printed++;
-                }
-            }
-        }
-        if(i <= height-2) { 
-            space++;
-            stars--;
-        }
-        else { 
-            space--;
-            stars++;
-        }
-        System.out.println();
-            //System.out.print("8");
-    }
 
     }
 }
